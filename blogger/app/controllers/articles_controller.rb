77cd393ef546d67d07f7bb3ef72cd631b555f92b
ElectroFish <chadoishere@gmail.com>
2015-04-07 20:30:15 +0300

    finished blog comments feature

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -6,6 +6,9 @@ class ArticlesController < ApplicationController
 
 	def show
 		@article = Article.find(params[:id])
+
+		@comment=Comment.new
+		@comment.article_id = @article.id
 	end
 
 	def new
@@ -16,6 +19,23 @@ class ArticlesController < ApplicationController
 		@article = Article.new(article_params)
 		@article.save
 
-		redirect_to article_path(@article)
+		redirect_to article_path(@article), notice: "Article '#{@article.title}' is Live!"
+	end
+
+	def destroy
+		Article.find(params[:id]).destroy
+
+		redirect_to articles_path, notice: "Article '#{@article.title}' Destroyed!"
+	end
+
+	def edit
+	 	@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		redirect_to article_path(@article), notice: "Article '#{@article.title}' Updated!"
 	end
 end
